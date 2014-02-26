163ef3b6d4b4ebfc9858497593e6c52079543d10
Matt Butcher <technosophos@gmail.com>
2014-02-25 20:18:18 -0700

    Renamed the helloWorld file and made minor changes.

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String who = "World";
+        if (args.length > 0) {
+          who = args[0];
+        }
+        System.out.printf("Hello, %s\n", who);
+    }
+
+}
